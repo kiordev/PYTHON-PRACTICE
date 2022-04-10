0dd3e6be53e83a5df6a4ed8478f9ca3f3682ce14
kiordev <kiordev@gmail.com>
2022-04-10 21:34:34 +0300

    Main Python File Application 0.1

---
+++ LoginAndPasswordForm.py
@@ -0,0 +1,8 @@
+# Login And Password Application
+# Creator: Alexandr Kior kiordev@gmail.com
+# Data: 10 APRIL 2022
+
+# DATABASE
+database_user_data = ["kiordev","12345"]
+
+user_data_login = input("Please enter you login: ")
