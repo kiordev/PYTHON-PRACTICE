2d7f040b652377ebf624a284c346e1bc739aeb70
kiordev <kiordev@gmail.com>
2022-04-10 18:51:00 +0300

    Add Array and Other

---
+++ Summary.py
@@ -0,0 +1,20 @@
+# Python Summary Document
+# Creator: Alexandr Kior kiordev@gmail.com
+# Data: 10 APRIL 22
+
+# ========= Array Functions =========
+
+# default.append(*something*) | Add element in the end of Array
+# default.insert(*index*, *something*) | Add element on index of Array
+# default.insert(*index*, *something*) | Add element on index of Array
+# default.extend([*something*]) | Add list of elements on in the end of Array
+# default.sort() | Sort Array for Up
+# default.reverse() | Make Array Mirrored
+# default.pop(index) | Delete index from Array (or last element without index)
+# default.remove(*somthing*) | Delete *something* from Array
+# default.clear | Clear Array
+# default.count(*something*) | Show count of *something* in Array
+# len(default) | Show Length of Array
+
+# ========= Other =========
+# exec(open('filename.py').read()) | Start another python script in terminal
