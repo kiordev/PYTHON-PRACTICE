4ec8c261edba6268ea2cafc826628bc0114d37d4
kiordev <kiordev@gmail.com>
2022-04-13 21:45:48 +0300

    Added new menu point

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 
 # Variables for Control
-user_choose_variable = 0
+user_choose_variable = None
 ProgrammIsActive = True
 
 
@@ -16,14 +16,14 @@ while ProgrammIsActive:
     print("===============")
     print("SeaBattleGame - 1")
     print("ModuleNumbersProgram - 2")
-    user_choose_variable = int(input("Enter the right variable"))
+    print("Human Data Creator - 3")
+    user_choose_variable = int(input("Enter the right variable: "))
     if user_choose_variable == 1:
         exec(open('SeaBattleGame.py').read())
-    if user_choose_variable == 2:
-        exec(open('ModuleNumberProgram.py').read())
-
-
-
-
-
+    elif user_choose_variable == 2:
+        exec(open('ModuleNumbersProgram.py').read())
+    elif user_choose_variable == 3:
+        exec(open('HumanDataCreator.py').read())
+    else:
+        print("Incorrect variant")
 
